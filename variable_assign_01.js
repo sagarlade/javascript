12a21bd5589cbd750272cc1bd07d8286049e7c16
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-04 22:00:45 -0700

    Create Variable_assign
    Variable_assign

---
+++ variable_assign_01.js
@@ -0,0 +1,38 @@
+var myFullName; //Declar
+console.log(myFullName);
+
+myFullName="Aditya Roy"; //Initialization
+console.log(myFullName);
+
+myFullName="raj birla";  //Update value
+console.log(myFullName);
+
+// Declare and Initialization
+
+var myLove;
+myLove="i love only JS";
+console.log(myLove);
+
+
+var firstName="Aditya";
+console.log("FirstName:- "+firstName);
+
+var lastName="Roy";
+console.log("LastName:- "+lastName);
+
+var age="22";
+console.log("Age:- "+age);
+
+var address="Sangola";
+console.log("Address:- "+address);
+
+var pincode="413308";
+console.log("Pincode:- "+pincode);
+
+var gender="Male";
+console.log("Gender:- "+gender);
+
+var isMarried="No";
+console.log("Married Status:-  "+isMarried);
+
+console.log(firstName, lastName, age, address, pincode, gender, isMarried);
