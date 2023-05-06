9f4a940b23b860c8bc6165a6a814f04f43d5da34
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-05 19:06:33 -0700

    hello.js
    Create hello.js file

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log("Sagar Lade");
+console.log("Ajanale Sangola");
+
+var firstName="Sagar";
+console.log(firstName);
+
+var age;
+console.log(age);
+
+age="22";
+console.log(age);
+
+age="23";
+console.log(age);
