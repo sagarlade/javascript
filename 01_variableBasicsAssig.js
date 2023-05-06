22b00a743bb1243676716a1ef5f9bacba5a9cee2
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-06 03:38:03 -0700

    Create varBaicsassig
    Create Data Type with var

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,35 @@
+// Data Types With variable
+
+var math=89;
+console.log("Maths Mark:", math, typeof math);
+var chemistry=76;
+console.log("Chemistry Mark:", chemistry, typeof chemistry);
+var marathi=96;
+console.log("Marathi Mark:", marathi, typeof marathi);
+
+console.log("================================");
+
+var mathTeacher="Sonalkar Sonali";
+console.log("Maths Teacher Name:", mathTeacher, typeof mathTeacher);
+var chemistryTeacher="Kolawale Swati";
+console.log("Chemistry Teacher Name:", chemistryTeacher, typeof chemistryTeacher);
+var marathiTeacher="Vibhuti Ajay";
+console.log("Marathi Teacher Name:", marathiTeacher, typeof marathiTeacher);
+
+console.log("================================");
+
+var isWorking=false;
+console.log("Working Status:", isWorking, typeof isWorking);
+var isMarried=true;
+console.log("Married Status:", isMarried, typeof isMarried);
+var isBachelor = false; 
+console.log("You are Bechelor:", isBachelor, typeof isBachelor);
+
+
+console.log("================================");
+
+var num=null;
+console.log("value:", num, typeof num);
+
+var a;
+console.log("value:", a, typeof a);
