0842a14a933abd295c6c7e84de5df2106c93e96e
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-06 03:32:33 -0700

    Create data types
    Create data types

---
+++ 02_typeOf.js
@@ -0,0 +1,20 @@
+var bankName;
+console.log(bankName, "  type:", typeof bankName);
+
+bankName="SBI Bank";
+console.log("Bank Name:", bankName , "  type:", typeof bankName);
+
+var marks="90%";
+console.log("Marks:", marks,  "  type:", typeof marks);
+
+var isWorking;
+console.log(isWorking,  "  type:", typeof isWorking);
+
+isWorking=true;
+console.log("Your Working:",isWorking,  "  type:", typeof isWorking);
+
+var totalCount="Hundred and Seven";
+console.log("Total Count: ", totalCount, " type:" , typeof totalCount);
+
+totalCount=107;
+console.log("Total Count: ", totalCount, " type:" , typeof totalCount);
