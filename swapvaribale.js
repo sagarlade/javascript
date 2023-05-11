d104ab67964361473dd79951fda398d732604dd6
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-11 03:59:46 -0700

    Update SwapVaribale
    Update SwapVaribale

--- swapvaribale.js
+++ swapvaribale.js
@@ -1,4 +1,13 @@
 
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("Before Swap...", "sweety: ", sweety, " cutie: ", cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After Swap...", "sweety: ", sweety, " cutie: ", cutie);
+
+
 var num1 = 100;
 var num2 = 200;
 var num3 = 300;
@@ -9,4 +18,4 @@ var temp = num1;
 num1 = num2;
 num2 = num3;
 num3 = temp;
-console.log("num1:", num1, " num2:", num2, "num3:", num3);
+console.log("After Swap..", "num1:", num1, " num2:", num2, "num3:", num3);
