1c1504c67356d82d8f1bc1c5420431f673723c9d
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-12 06:08:44 -0700

    string basics
    string basics

---
+++ 04_stringBasics.js
@@ -0,0 +1,14 @@
+function stringBasics(){
+    var company="Microsoft";
+    console.log("My dream company is :", company);
+    var hobby1="Reading";
+    var hobby2="Playing a musical instrument";
+    var hobby3="Board games";
+    console.log("My Hobbies are: ", hobby1, hobby2, hobby3);
+    var hobby1length=hobby1.length;
+    var hobby2length=hobby2.length;
+    var hobby3length=hobby3.length;
+    var sum=hobby1length+hobby2length+hobby3length;
+    console.log("Sum of total number of characters in Hobby is: ", sum);
+}
+stringBasics();
