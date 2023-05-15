beb7db3125a6182a72d6ad3e90037377fc6b12ca
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-14 20:51:46 -0700

    Operators
    Math Operators

---
+++ 05_operatorA.js
@@ -0,0 +1,31 @@
+function squareOfWordLength(str){
+    var wordLength=str.length;
+    console.log(`Length of Word is ==> ${wordLength}`);
+
+    var squareOfWord=wordLength*wordLength;
+    console.log(`Square of Word Length ==> ${squareOfWord}`);
+}
+squareOfWordLength("Javascript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Developer Smart");
+
+console.log(`-------------------------------------------------------`);
+function developer(){
+    
+    var developer="I am Angular Deveoper";
+    var lengthOfStr=developer.length;
+    console.log(`Length of String ==> ${lengthOfStr}`);
+
+    var wordOfStr=developer.split(" ");
+    var splitStrLength=wordOfStr.length;
+
+    console.log(`Available of Words in String ==> ${splitStrLength}`);
+
+    var division=lengthOfStr / splitStrLength;
+    console.log(`Division of string length and total words is ==> ${division}`);
+
+    var multipleStr=lengthOfStr * splitStrLength;
+    console.log(`Multiplication of string length and total words is ==> ${multipleStr}`);
+
+}
+developer();
