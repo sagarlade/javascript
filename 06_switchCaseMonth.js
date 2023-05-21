717d499346152230ad21ea3ef2c59cf792663d13
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-21 00:38:14 -0700

    SwitchCaseMonth.js
    SwitchCaseMonth.js

---
+++ 06_switchCaseMonth.js
@@ -0,0 +1,45 @@
+function monthOfYear(monthNumber) {
+  switch (monthNumber) {
+    case 1:
+      console.log(`Month is: January as Month number is ${monthNumber}`);
+      break;
+    case 2:
+      console.log(`Month is: February as Month number is ${monthNumber}`);
+      break;
+    case 3:
+      console.log(`Month is: March as Month number is ${monthNumber}`);
+      break;
+    case 4:
+      console.log(`Month is: April as Month number is ${monthNumber}`);
+      break;
+    case 5:
+      console.log(`Month is: May as Month number is ${monthNumber}`);
+      break;
+    case 6:
+      console.log(`Month is: June as Month number is ${monthNumber}`);
+      break;
+    case 7:
+      console.log(`Month is: July as Month number is ${monthNumber}`);
+      break;
+    case 8:
+      console.log(`Month is: August as Month number is ${monthNumber}`);
+      break;
+    case 9:
+      console.log(`Month is: September as Month number is ${monthNumber}`);
+      break;
+    case 10:
+      console.log(`Month is: October as Month number is ${monthNumber}`);
+      break;
+    case 11:
+      console.log(`Month is: November as Month number is ${monthNumber}`);
+      break;
+    case 12:
+      console.log(`Month is: December as Month number is ${monthNumber}`);
+      break;
+    default:
+      console.log(`Invalid input ==>  ${monthNumber}`);
+      break;
+  }
+}
+monthOfYear(2);
+
