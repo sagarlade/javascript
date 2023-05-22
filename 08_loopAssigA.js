db219624f141eba2be80f460a7ff38a30c2012a1
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-05-21 18:39:11 -0700

    update 08_loopAssigA
    update 08_loopAssigA

--- 08_loopAssigA.js
+++ 08_loopAssigA.js
@@ -2,51 +2,54 @@
 
 console.log(`1. Count Total number of Vowels`);
 function countVowels(str) {
-  var vowels = "aeiouAEIOU";
+  var vowels = 'aeiouAEIOU';
   var count = 0;
-
+  
   for (var i = 0; i < str.length; i++) {
     if (vowels.includes(str[i])) {
       count++;
     }
   }
-
+  
   return count;
 }
 
 var sentence = "I am very good IT Developer";
 var vowelCount = countVowels(sentence);
-console.log("Total number of vowels:", vowelCount);
+console.log("Total number of vowels:", vowelCount);
 
 console.log(`----------------------------------------------------`);
 console.log(`2. Calculate the sum of the cubes of numbers from 1 to 5 `);
 
-function sumOfCube(n) {
+function sumOfCube() {
   var sum = 0;
-  for (var i = 1; i <= n; i++) {
-    sum += Math.pow(i, 3);
+  for (var i = 1; i <= 5; i++) {
+    sum += i ** 3;
   }
   console.log(sum);
 }
+sumOfCube();
 
-var n = 5;
-var result = sumOfCube(n);
-console.log(`Sum of Cubes: ${result}`);
 
 console.log(`----------------------------------------------------`);
+
 console.log(`3. Log only Odd Positioned Chars on console and do not consider empty spaces`);
 function oddPositionedChars(str) {
-    let oddChars=" ";
-    for (let i = 0; i < str.length; i += 2) {
-      if (str[i] !== ' ') {
-        oddChars +=str[i];
-      }
+  let oddChars = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0 && str[i] !== ' ') {
+      oddChars += str[i];
     }
-    console.log(oddChars);
   }
 
-var str1 = "Hard work always pays back";
-oddPositionedChars(str1);
+  return oddChars;
+}
 
-var str2 = "Soon I will be Angular IT Champ";
-oddPositionedChars(str2);
+const str1 = "Hard work always pays back";
+const result1 = oddPositionedChars(str1);
+console.log(result1); 
+
+const str2 = "Soon I will be Angular IT Champ";
+const result2 = oddPositionedChars(str2);
+console.log(result2); 
