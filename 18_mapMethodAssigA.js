fa5a6eb1accd6621638e3e46c536f2ed980bf582
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-06-06 02:25:31 -0700

    18_mapMethodAssigA.js
    18_mapMethodAssigA.js

--- 18_mapMethodAssigA.js
+++ 18_mapMethodAssigA.js
@@ -1,16 +1,22 @@
 const arrayNumbers = [20, 11, 40, 25, 23, 11, 9, 31, 60, 2, 19];
 
 console.log("====== Add 10 to each element and log the new array =======");
-const newArray1 = arrayNumbers.map((num) => num + 10);
-console.log("New Array (Add 10):", newArray1);
-
-// 2. Square each array element and log it on the console
+const newArray1 = arrayNumbers.map((num) => {
+    return num + 10;
+});
+console.log("New Array with Adding 10 in Each Element==> ", newArray1);
 
 console.log("========= Square each array element ===========");
-const newArray2 = arrayNumbers.map((num) => num * num);
-console.log("New Array (Square):", newArray2);
+const newArray2 = arrayNumbers.map((num) => {
+    return num * num;
+});
+console.log("Square of each element ==>", newArray2);
 
-console.log("========= Add the index value to its corresponding array element and log the new array ===========");
+console.log(
+  "========= Add the index value to its corresponding array element and log the new array ==========="
+);
 
-const newArray3 = arrayNumbers.map((num, index) => num + index);
-console.log("New Array (Add Index):", newArray3);
+const newArray3 = arrayNumbers.map((num, index) =>{
+    return num + index;
+} );
+console.log("Adding index to corresponding element:", newArray3);
